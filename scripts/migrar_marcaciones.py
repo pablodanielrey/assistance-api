3488d286bba452812742aeba57ff96b02a8f3c20
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-03-18 01:40:20 -0300

    acomode el script para que sea mas rápido migrando

--- scripts/migrar_marcaciones.py
+++ scripts/migrar_marcaciones.py
@@ -14,9 +14,15 @@ if __name__ == '__main__':
     ))
     cur = conn.cursor()
     try:
-        cur.execute('select marcacion from marcacion order by marcacion asc limit 1')
-        fecha = cur.fetchone()[0]
-        logging.info(fecha)
+        cur.execute('select usuario_id, marcacion from marcacion')
+        indice = {}
+        for m in cur.fetchall():
+            try:
+                indice[m[0]].append(m[1])
+            except KeyError as e:
+                indice[m[0]] = [m[1]]
+
+        logging.debug('indice generado')
 
         conn2 = psycopg2.connect("host='{}' user='{}' password='{}' dbname={}".format(
             os.environ['OLD_ASSISTANCE_DB_HOST'],
@@ -31,16 +37,17 @@ if __name__ == '__main__':
             cur2.execute('select id, user_id, verifymode, log from assistance.attlog')
             for m in cur2.fetchall():
                 count = count + 1
-                if m[3] < fecha:
-                    cur.execute('insert into marcacion (id, usuario_id, dispositivo_id, tipo, marcacion) values (%s,%s,%s,%s,%s)', (m[0], m[1], '35fa769e-7146-4e84-a10f-f41e1e07685b', m[2], m[3]))
-                else:
-                    cur.execute('select 1 from marcacion where marcacion = %s and usuario_id = %s', (m[3],m[1]))
-                    if cur.rowcount <= 0:
-                        cur.execute('insert into marcacion (id, usuario_id, dispositivo_id, tipo, marcacion) values (%s,%s,%s,%s,%s)', (m[0], m[1], '35fa769e-7146-4e84-a10f-f41e1e07685b', m[2], m[3]))
-                if count > 10000:
-                    logging.debug(count)
-                    count = 0;
+                if m[1] in indice and m[3] in indice[m[1]]:
+                    continue
+                #cur.execute('select 1 from marcacion where marcacion = %s and usuario_id = %s', (m[3],m[1]))
+                #if cur.rowcount <= 0:
+                cur.execute('insert into marcacion (id, usuario_id, dispositivo_id, tipo, marcacion) values (%s,%s,%s,%s,%s)', (m[0], m[1], '35fa769e-7146-4e84-a10f-f41e1e07685b', m[2], m[3]))
+                logging.debug('agregando {}'.format(m))
+                count = count + 1
+                if count > 100:
+                    count = 0
                     conn.commit()
+            conn.commit()
 
         finally:
             conn2.close()
