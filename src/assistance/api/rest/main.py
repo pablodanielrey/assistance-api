5344d2916376957a225a7bb284212c41bced13fb
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-05-29 21:34:42 -0300

    ahora en vez de tirar un error 500 debe tirar un error unauthorized

--- src/assistance/api/rest/main.py
+++ src/assistance/api/rest/main.py
@@ -50,11 +50,11 @@ def usuarios(uid=None, token=None):
     w = Warden('https://api.econo.unlp.edu.ar/warden/api/v1.0', client_id, client_secret)
     access = w.check_access(token, 'rn:assistance:users', 'list')
     if not access:
-        raise Exception('no tiene los permisos suficientes')
+        return ('no tiene los permisos suficientes', 403)
     
     prof = w.has_profile(token, 'admin-assistance')
     if not prof or prof['profile'] == False:
-        raise Exception('no tiene los permisos suficientes')
+        return ('no tiene los permisos suficientes', 403)
 
     search = request.args.get('q',None)
     offset = request.args.get('offset',None,int)
