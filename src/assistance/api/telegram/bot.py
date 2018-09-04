8230c8954b003b0a8c2f781bfc2693edca2f15ca
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-09-03 21:18:34 -0300

    corregi un pcoo el codigo para que tenga en cuenta los flujos de comunicación que analizamos. solo falta que entienda los callbacks de los botones

--- src/assistance/api/telegram/bot.py
+++ src/assistance/api/telegram/bot.py
@@ -44,6 +44,7 @@ from dateutil import parser
 import pytz
 import os
 import json
+import inspect
 
 from telegram.ext import Updater
 from telegram.ext import CommandHandler, CallbackQueryHandler
@@ -62,81 +63,67 @@ bot_name = os.environ.get('TELEGRAM_BOT_NAME')
 bot_username = os.environ.get('TELEGRAM_BOT_USERNAME')
 TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 
-WEB_URL = os.environ.get('ASSISTANCE_URL', 'http://localhost')
-
 print('Iniciando bot usando token : {}'.format(TOKEN))
 
 def _pedir_telefono(bot, cid):
-    telefono = """
-<pre>
-Para poder continuar necesitamos verificar su número telefónico.
-Por favor haga click en el siguiente botón.
-Gracias.
-</pre>"""
+    telefono = inspect.cleandoc("""
+                    Para poder continuar necesitamos verificar su número telefónico.
+                    Por favor haga click en el siguiente botón.
+                    Gracias.""")
     bot.send_message(
         chat_id=cid,
         text=telefono, 
-        parse_mode=ParseMode.HTML, 
+        #parse_mode=ParseMode.HTML, 
         reply_markup=ReplyKeyboardMarkup([[KeyboardButton(text='Enviar número', request_contact=True, request_location=True)]])
         )
+
+def _generar_codigo(bot, cid):
+    code = hashlib.sha1('{}_{}'.format(datetime.datetime.now(),cid).encode('utf-8')).hexdigest()[:5]
+    kc = 't_auth_{}'.format(code)
+    r.hset(kc,'chat_id',cid)
+    r.expire(kc,60 * 5)         # 5 minutos el codigo de autorizacion
+
+    template = inspect.cleandoc("""Por favor ingrese a la aplicación web de asistencia y escriba el siguente codigo
+                                en la seccion de Telegram.
+                                Codigo: {}""".format(code))
+
+    logging.info(template)
+    bot.send_message(chat_id=cid, text=template)
+
+def _enviar_seleccion_notificar(bot, cid):
+    iniciar_keyboard = InlineKeyboardButton(text="Recibir notificaciones", callback_data='1')
+    finalizar_keyboard = InlineKeyboardButton(text="No recibir notificaciones", callback_data='2')
+    reply_markup = ReplyKeyboardMarkup([[iniciar_keyboard, finalizar_keyboard]])
+    bot.send_message(chat_id=cid, text='Debe seleccionar si desea recibir los eventos o no', reply_markup=reply_markup)
 
 def inicio(bot, update, args=[]):
     cid = update.message.chat_id
     logging.info('cliente conectado {}'.format(cid))
 
-    k = 't_chat_id_{}'.format(cid)
-    if not r.hexists(k,'uid'):
-        code = hashlib.sha1('{}_{}'.format(datetime.datetime.now(),cid).encode('utf-8')).hexdigest()[:5]
-        kc = 't_auth_{}'.format(code)
-        r.hset(kc,'chat_id',cid)
-
-        template = """
-<pre>
-Por favor ingrese a la aplicación web de asistencia y escriba el siguente codigo
-en la seccion de Telegram.
-Codigo: {}
-</pre>
-""".format(code)
-
-        logging.info(template)
-        bot.send_message(chat_id=cid, text=template, parse_mode=ParseMode.HTML)
+    t_chat_id = 't_chat_id_{}'.format(cid)
+    if not r.hexists(t_chat_id,'uid'):
+        _generar_codigo(bot, cid)
         return
 
-
-        url = '{}/telegram/{}'.format(WEB_URL, code)
-        template = """
-<pre>
-Para activar su usuario por favor haga click
-</pre>
-<a href="{}">aqui</a>
-""".format(url)
-        
-        logging.info('enviando')
-        logging.info(template)
-        bot.send_message(chat_id=cid, text=template, parse_mode=ParseMode.HTML)
-        return
-
-
-    """
-        Ya tenemos asociado el chat_id con un usuario.
-        por lo que se continua con el proceso de registro del usuario.
-    """
-
-    uid = r.hget(k,'uid')
-    kt = 'telegram_{}'.format(uid)
-    usr = r.hgetall(kt)
+    uid = r.hget(t_chat_id,'uid')
+    telegram_uid = 'telegram_{}'.format(uid)
+    usr = r.hgetall(telegram_uid)
     if not usr:
-        ''' situación anómala. desregistro completamente al usuario, el chat_id '''
-        r.hdel(k,'uid')
+        ''' situación anómala. desregistro completamente al usuario. '''
+        r.hdel(t_chat_id,'uid')
         bot.send_message(chat_id=cid, text='Error. Por favor regítrese nuevamente')
+        _generar_codigo(bot, cid)
         return
 
     bot.send_message(chat_id=cid, text='Bienvenido {} {}'.format(usr['u_nombre'], usr['u_apellido']))
-    
 
+    if 't_telefono' not in usr or not usr['t_telefono']:
+        _pedir_telefono(bot, cid)
+        return
+
+    _enviar_seleccion_notificar(bot, cid)
 
 def contact_callback(bot, update):
-    ''' recibo la info del contacto. respuesta al mensaje anterior '''
     cid = update.effective_message.chat_id
     contact = update.effective_message.contact
     
@@ -145,24 +132,18 @@ def contact_callback(bot, update):
         inicio(bot, update)
         return
 
-    k = 'telegram_{}'.format(uid)
-    tusr = r.hgetall(k)
-    if not tusr:
+    telegram_uid = 'telegram_{}'.format(uid)
+    if not r.hexists(telegram_uid,'t_chat_id'):
         inicio(bot, update)
         return
 
-    r.hset(k, 't_telefono', contact.phone_number)
-    r.hset(k, 't_nombre', contact.first_name)
-    r.hset(k, 't_apellido', contact.last_name)
-    r.hset(k, 't_usuario_id', contact.user_id)
+    r.hset(telegram_uid, 't_telefono', contact.phone_number)
+    r.hset(telegram_uid, 't_nombre', contact.first_name)
+    r.hset(telegram_uid, 't_apellido', contact.last_name)
+    r.hset(telegram_uid, 't_usuario_id', contact.user_id)
+    r.hset(telegram_uid, 'notificar', 0)
 
     _enviar_seleccion_notificar(bot, cid)
-
-def _enviar_seleccion_notificar(bot, cid):
-    iniciar_keyboard = InlineKeyboardButton(text="Recibir notificaciones", callback_data='1')
-    finalizar_keyboard = InlineKeyboardButton(text="No recibir notificaciones", callback_data='2')
-    reply_markup = ReplyKeyboardMarkup([[iniciar_keyboard, finalizar_keyboard]])
-    bot.send_message(chat_id=cid, text='Debe seleccionar si desea recibir los eventos o no', reply_markup=reply_markup)
 
 def button(bot, update):
     query = update.callback_query
@@ -188,11 +169,11 @@ def button(bot, update):
     bot.sent_message(chat_id=cid, text='No entiendo')
 
 def status(bot, update):
+    ''' método de debug para los admins '''
     cid = update.message.chat_id
     if not r.hexists('t_chat_id_{}'.format(cid), 'uid'):
         bot.send_message(chat_id=cid, text='No permitido')
         return
-
     for k in r.keys('*'):
         bot.send_message(chat_id=cid, text=k)
         logging.info(k)
@@ -200,7 +181,7 @@ def status(bot, update):
 
 def text_callback(bot, update):
     cid = update.message.chat_id
-    bot.send_message(chat_id=cid, text='No entiendo')
+    bot.send_message(chat_id=cid, text='No entiendo texto')
 
 def _obtener_correo(mails):
     if not mails:
@@ -231,35 +212,28 @@ def _procesar_cola_marcaciones(bot, timezone=None):
                 nombre = l['usuario']['nombre']
                 apellido = l['usuario']['apellido']
 
-                template = """
-<pre>
-Dni: {}
-Nombre: {}
-Apellido: {}
-Fecha: {}
-Hora: {}
-Tipo Marcación: {}
-Reloj: {}
-Correo: {}
-</pre>
-                """.format(dni, nombre, apellido,                    
-                    fecha_hora.date(),
-                    fecha_hora.time(),
-                    l['log']['tipo'],
-                    l['log']['dispositivo_id'],
-                    #_obtener_correo(l['usuario']['mails']))
-                    "en la nueva versión"
-                )
-
-                logging.info('Notificando')
+                template = inspect.cleandoc("""<pre>
+                                            Dni: {}
+                                            Nombre: {}
+                                            Apellido: {}
+                                            Fecha: {}
+                                            Hora: {}
+                                            Tipo Marcación: {}
+                                            Reloj: {}
+                                            Correo: {}
+                                            </pre>""".format(dni, nombre, apellido,
+                                                            fecha_hora.date(),
+                                                            fecha_hora.time(),
+                                                            l['log']['tipo'],
+                                                            l['log']['dispositivo_id'],
+                                                            "en la nueva versión"))
+                logging.info('Notificando')                
                 logging.info(usr)
                 logging.info(template)
-
                 bot.send_message(chat_id=cid, text='{}'.format(template), parse_mode=ParseMode.HTML)
             else:
                 logging.info('cliente no quiere ser notificado')
                 logging.info(usr)
-
 
 def _procesar_cola_autorizacion(bot):
     uid = True
@@ -269,10 +243,16 @@ def _procesar_cola_autorizacion(bot):
             continue
         usr = r.hgetall('telegram_{}'.format(uid))
         cid = usr['t_chat_id']
-        template = "Bién!!! {} {} se ha registrado correctamente".format(usr['u_nombre'],usr['u_apellido'])
+        template = "{} {} se ha registrado correctamente".format(usr['u_nombre'],usr['u_apellido'])
         logging.info(template)
         bot.send_message(chat_id=cid, text=template)
-    
+
+        if 't_telefono' not in usr or not usr['t_telefono']:
+            _pedir_telefono(bot, cid)
+            return
+            
+        _enviar_seleccion_notificar(bot, cid)
+
 
 def callback_minute(bot, job):
     timezone = pytz.timezone('America/Argentina/Buenos_Aires')
