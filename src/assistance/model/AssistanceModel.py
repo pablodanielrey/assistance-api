88007df485a2152466e46ee0dc6f288f65105d8f
Miguel Macagno <miguel.macagno@gmail.com>
2018-09-07 20:55:24 -0300

    Prueba

--- src/assistance/model/AssistanceModel.py
+++ src/assistance/model/AssistanceModel.py
@@ -211,7 +211,7 @@ class AssistanceModel:
         hora_salida = None
         horario_segundos = 0
         if reporte.horario:
-            (hora_entrada, hora_salida) = reporte.horario.obtenerInicioFin(reporte.fecha,tzone)
+            (hora_entrada, hora_salida) = reporte.horario.obtenerInicioFin(reporte.fecha_inicial, tzone)
             horario_segundos = reporte.horario.cantidadDeSegundos()
 
         #proceso las justificaciones para el formato esperado:
