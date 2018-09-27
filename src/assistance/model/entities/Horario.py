52043d259177408d9ac7fbee5d60428fe3fdf9ba
Miguel Macagno <miguel.macagno@econo.unlp.edu.ar>
2018-09-27 08:05:47 -0300

    Prueba de print

--- src/assistance/model/entities/Horario.py
+++ src/assistance/model/entities/Horario.py
@@ -49,5 +49,7 @@ class Horario(Base):
             timezone = pytz.timezone(timezone)
         dt = datetime.combine(fecha, time(0), timezone)
         inicio = dt + timedelta(seconds=self.hora_entrada)
+        print('Este es el inicio ------->', inicio)
         fin = dt + timedelta(seconds=self.hora_salida)
+        print('Este es el fin ------->', fin)
         return (inicio, fin)
