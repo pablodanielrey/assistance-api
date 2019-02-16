4aeef67ceae064935976efbacf6c110fbed388b5
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-02-16 14:17:11 -0300

    ahora esta corriendo el modo debug sin problemas.

---
+++ src/assistance/api/rest/users.py
@@ -0,0 +1,27 @@
+
+def get():
+    pass
+
+def profile():
+    pass
+
+def report():
+    pass
+
+def justifications():
+    pass
+
+def timetable():
+    pass
+
+def delete_timetable():
+    pass
+
+def historic_timetable():
+    pass
+
+def logs():
+    pass
+
+def crear_log():
+    pass
