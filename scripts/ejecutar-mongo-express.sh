34ec7c6b03b08be66f997f42b124dc4185de96c2
Miguel Macagno <miguel.macagno@econo.unlp.edu.ar>
2019-02-22 12:13:17 -0300

    Nuevos scripts de migracion de sistemas de asistencia

--- scripts/ejecutar-mongo-express.sh
+++ scripts/ejecutar-mongo-express.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -ti --rm --name mongo-express --add-host mongo:192.168.0.3 -p 8081:8081 mongo-express
+docker run -ti --rm --name mongo-express --add-host mongo:163.10.56.62 -p 8081:8081 mongo-express
