96600ca66050100dcb63ef588900fc1b9ede8a10
Miguel Macagno <miguel.macagno@econo.unlp.edu.ar>
2019-03-07 12:57:12 -0300

    Se agregan scripts basicos para activar-desactivar reloj y se agregan pruebas de obtener usuarios

--- scripts/relojes/obtener_usuarios.py
+++ scripts/relojes/obtener_usuarios.py
@@ -6,7 +6,7 @@ import logging
 logging.getLogger().setLevel(logging.INFO)
 
 
-ip_address = '163.10.56.204'
+ip_address = '163.10.56.25'
 machine_port = 4370
 
 
@@ -17,6 +17,14 @@ z.disable_device()
 z.read_all_user_id()
 z.read_all_fptmp()
 z.print_users_summary()
+z.read_att_log()
+z.print_attlog()
+
+logging.info(z.users)
+for u in z.users.values():
+    logging.info(u.user_id)
+    logging.info(u.user_name)
+    logging.info(u.user_password)
 
 z.enable_device()
 z.disconnect()
