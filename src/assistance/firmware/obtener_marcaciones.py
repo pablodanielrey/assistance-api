d824a0746618fafe6e14b4b4947af88abeb2cd87
Miguel Macagno <miguel.macagno@gmail.com>
2019-03-07 19:51:14 -0300

    Se termina primer aproximacion a obtener marcaciones a json

--- src/assistance/firmware/obtener_marcaciones.py
+++ src/assistance/firmware/obtener_marcaciones.py
@@ -6,7 +6,6 @@ import logging
 import json
 logging.getLogger().setLevel(logging.INFO)
 
-
 ip_address = '163.10.56.25'
 machine_port = 4370
 
@@ -15,15 +14,20 @@ z.connect_net(ip_address, machine_port)
 z.disable_device()
 
 z.read_att_log()
-z.print_attlog()
 
-
+datos = {}
+datos['logs'] = []
 for l in z.att_log:
     print("User: {} Marcacion: {}".format(l.user_id, l.att_time))
+    datos['logs'].append({
+            'usuario' : l.user_id,
+            'tipo' : l.ver_type,
+            'marcacion' : '{}'.format(l.att_time),
+            'estado' : l.ver_state
+    })
 
-
-with open('/tmp/marcaciones.json', 'w') as outfile:
-    json.dump(data, outfile)
+with open('/tmp/logs.json', 'w') as outfile:
+    json.dump(datos, outfile)
 
 z.enable_device()
 z.disconnect()
--- src/assistance/firmware/obtener_usuarios.py
+++ src/assistance/firmware/obtener_usuarios.py
@@ -3,6 +3,7 @@ import os.path
 import pyzk.pyzk as pyzk
 from pyzk.zkmodules.defs import *
 import logging
+import json
 logging.getLogger().setLevel(logging.INFO)
 
 
@@ -14,15 +15,21 @@ z.connect_net(ip_address, machine_port)
 z.disable_device()
 
 z.read_all_user_id()
-z.read_all_fptmp()
 z.print_users_summary()
-z.read_att_log()
-z.print_attlog()
 
+datos = {}
+datos['usuarios'] = []
 for u in z.users.values():
-    logging.info(u.user_id)
-    logging.info(u.user_name)
-    logging.info(u.user_password)
+    datos['usuarios'].append({
+        'usuario': u.user_id,
+        'password': u.user_password,
+        'admin_level': u.admin_level,
+        'activado': u.not_enabled,
+    })
+
+with open('/tmp/usuarios.json', 'w') as outfile:
+    json.dump(datos, outfile)
+
 
 z.enable_device()
 z.disconnect()
