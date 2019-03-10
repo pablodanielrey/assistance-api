aae4a5417a9ad04aedb33280c854918c1827d909
Miguel Macagno <miguel.macagno@gmail.com>
2019-03-10 17:25:00 -0300

    Se agrega script para actualizar la hora del dispositivo

---
+++ src/assistance/firmware/actualizar_hora.py
@@ -0,0 +1,28 @@
+import time
+from datetime import datetime
+import os.path
+import pyzk.pyzk as pyzk
+from pyzk.zkmodules.defs import *
+import logging
+logging.getLogger().setLevel(logging.INFO)
+
+
+ip_address = '163.10.56.25'
+machine_port = 4370
+
+z = pyzk.ZKSS()
+try:
+    z.connect_net(ip_address, machine_port)
+    z.disable_device()
+
+    print("Hora en dispositivo: {}".format(z.get_device_time()))
+    print("Hora actualizada:    {}".format(datetime.now()))
+    print("Actualizando Hora...")
+    z.set_device_time(datetime.now())
+    print("Hora en dispositivo: {}".format(z.get_device_time()))
+    
+    z.enable_device()
+finally:
+    z.disconnect()
+
+
