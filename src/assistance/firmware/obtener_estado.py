e86edac2d70169a6937f8d652810645d89635f14
Miguel Macagno <miguel.macagno@gmail.com>
2019-03-10 17:43:22 -0300

    Se agrega nuevo script para informe de estado del dispositivo

---
+++ src/assistance/firmware/obtener_estado.py
@@ -0,0 +1,45 @@
+import pyzk.pyzk as pyzk
+import pyzk.zkmodules.defs as defs
+
+ip_address = '163.10.56.25'
+machine_port = 4370
+
+z = pyzk.ZKSS()
+try:
+    z.connect_net(ip_address, machine_port)
+    z.disable_device()
+
+    print("ESTADISTICAS DEL EQUIPO")
+    stat_keys = defs.get_status_keys()
+    res = z.get_device_status(dict(zip(stat_keys, [-1]*len(stat_keys))))
+    for k in res:
+        print("{0} = {1}".format(k, res[k]))
+    print("---------------------------------------------------")
+    
+    print("CONTADORES INTERNOS")
+    res = z.get_device_status({'attlog_count': -1, 'user_count': -1, 'admin_count': -1})
+    for k in res:
+        print("{0} = {1}".format(k, res[k]))
+    print("---------------------------------------------------")
+    
+    print("USUARIOS CON CLAVE")
+    res = z.read_status(defs.STATUS['pwd_count'])
+    print("{0} = {1}".format('pwd_count', res))
+    print("---------------------------------------------------")
+    
+    print("INFORMACION DE DISPOSITIVO")
+    print('Vendor = ' + z.get_vendor())
+    print('Product code = |{0}|'.format(z.get_product_code()))
+    print('Product time = |{0}|'.format(z.get_product_time()))
+    print('card function = |{0}|'.format(z.get_cardfun()))
+    print('User max id width = |{0}|'.format(z.get_device_info('~PIN2Width')))
+    print('Firmware version = |{0}|'.format(z.get_firmware_version()))
+    print("---------------------------------------------------")
+    
+    print("ESTADO DEL DISPOSITIVO")
+    print('Device state = |{0}|'.format(z.get_device_state()))
+
+    z.enable_device()
+finally:
+    z.disconnect()
+
