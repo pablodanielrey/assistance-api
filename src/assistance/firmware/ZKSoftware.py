9dcfabc76ac99541c6d80eba8bf1dc69ae814e25
Miguel Macagno <miguel.macagno@econo.unlp.edu.ar>
2019-03-11 14:10:16 -0300

    Se crea decodificdor nuevo

--- src/assistance/firmware/ZKSoftware.py
+++ src/assistance/firmware/ZKSoftware.py
@@ -1,5 +1,8 @@
 import struct
 import binascii
+import logging
+logging.getLogger().setLevel(logging.INFO)
+
 
 """Codigo comun de ayuda y pruebas para propia implementacion de funciones de zk"""
 
@@ -85,3 +88,101 @@ def subir_huella(z, user_sn, fp, fp_index, fp_flag):
 
     # refresh data
     z.refresh_data()
+
+
+
+def decodificar_str(s):
+    i = 0
+    while i < len(s) and s[i] != 0x00:
+        i += 1
+    return s[:i]
+
+def decodificar_info_usuario(data):
+    (sn,permission, passw, name, card, group, tz, tz1, tz2, tz3, uid) = struct.unpack('<HB8s24sIBHHHH9s15x',data)
+    return {
+        'sn': sn,
+        'permission': permission,
+        'password': passw.decode('ascii'),
+        'name': name.decode('ascii'),
+        'card': card,
+        'group': group,
+        'tz': tz,
+        'tz1': tz1,
+        'tz2': tz2,
+        'tz3': tz3,
+        'uid': decodificar_str(uid).decode('ascii')
+    }
+
+
+
+def leer_todos_usuarios_id(z):
+    """
+    Requests all the users info, except the fingerprint templates.
+    :return: None. Stores the users info in the ZKUsers dict.
+    """
+    z.send_command(cmd=0x05df, data=bytearray.fromhex('0109000500000000000000'))
+
+    # receive dataset with users info
+    users_dataset = z.recv_long_reply()
+    total_size_dataset = len(users_dataset)
+
+    # clear the users dict
+    z.users = {}
+
+    # skip first 4 bytes (size + zeros)
+    i = 4
+    while i < total_size_dataset:
+        datos = users_dataset[i:i+72]
+        usuario = decodificar_info_usuario(datos)
+        i = i + 72
+        logging.info(usuario)
+
+        """
+        #print(users_dataset[i:i+72])
+        # extract serial number
+        user_sn = struct.unpack('<H', users_dataset[i:i+2])[0]
+
+        # extract permission token
+        perm_token = users_dataset[i+2]
+
+        # extract user password, if it is invalid, stores ''
+        if users_dataset[i+3] != 0x00:
+            password = users_dataset[i + 3:i + 11]
+            # remove trailing zeros
+            password = password.decode('ascii').replace('\x00', '')
+
+        else:
+            password = ''
+
+        # extract user name
+        user_name = users_dataset[i+11:i+35].decode('ascii')
+
+        # remove non printable chars
+        user_name = user_name.replace('\x00', '')
+
+        # extract card number
+        card_no = struct.unpack('<I', users_dataset[i+35:i+39])[0]
+
+        # extract group number
+        group_no = users_dataset[i+39]
+
+        # extract user timezones if they exists
+        if struct.unpack('<H', users_dataset[i+40:i+42])[0] == 1:
+            user_tzs = [0]*3
+            user_tzs[0] = struct.unpack('<H', users_dataset[i+42:i+44])[0]
+            user_tzs[1] = struct.unpack('<H', users_dataset[i+44:i+46])[0]
+            user_tzs[2] = struct.unpack('<H', users_dataset[i+46:i+48])[0]
+        else:
+            user_tzs = []
+
+        # extract the user id
+        user_id = users_dataset[i+48:i+57].decode('ascii')
+        prueba = struct.unpack('<H', users_dataset[i+48:i+57])
+        print("User Id: {}".format(user_id))
+        print('Data Set de user_id: {}'.format(users_dataset[i+48:i+57]))
+        
+        # remove non printable chars
+        user_id = user_id.replace('\x00', '')
+        # every user entry is 72 bytes long
+        i += 72
+        """
