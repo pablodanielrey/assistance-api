64f26fcc6d722f35b3de6972b5b2738ff3283106
migue <miguel.macagno@presi.unlp.edu.ar>
2019-03-11 14:48:04 -0300

    Se corrige descripcion del script

--- src/assistance/firmware/obtener_log_operaciones.py
+++ src/assistance/firmware/obtener_log_operaciones.py
@@ -4,7 +4,7 @@ import pyzk.pyzk as pyzk
 from pyzk.zkmodules.defs import *
 from ZKSoftware import *
 
-"""Obtiene la lista de usuarios que estan creados en el reloj biometrico y sus huellas"""
+"""Obtiene el log actual de operaciones del reloj"""
 
 ip_address = '163.10.56.25'
 machine_port = 4370
