f16e40651793ff47a757edf26fd4808f31248c2b
pablo <Pablo Daniel Rey>
2022-06-04 13:12:22 -0300

    actualizo para que tome los datos por parámetro

--- src/assistance/firmware/scriptsv2/get_all_data.py
+++ src/assistance/firmware/scriptsv2/get_all_data.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 from zk import ZK, const
 
 import base64
@@ -50,36 +51,44 @@ def assign_to_user(fp: Fingerprint, users: list[CompleteUser]):
     raise Exception(f'User {fp.uid} not found for fingerprint {fp.uid}')
 
 
-conn = None
-zk = ZK('172.25.110.29', port=4370, timeout=5)
-print('Connecting to device ...')
-conn = zk.connect()
-try:
-    
-    data = Data()
-
-    conn.enable_device()
-    print('Firmware Version: : {}'.format(conn.get_firmware_version()))
-    conn.test_voice()
-
-    users = conn.get_users()
-    for user in users:
-        cuser = CompleteUser.from_orm(user)
-        data.users.append(cuser)
-        print(cuser.json())
-
-    fps = conn.get_templates()
-    for fp in fps:
-        finger = Fingerprint.from_orm(fp)
-        print(finger.json())
-        assign_to_user(finger, data.users)    
-
-    conn.enable_device()
-
-    with open('users.json', 'w') as f:
-        f.write(data.json())
 
 
-except Exception as e2:
-    logging.exception(e2)
-    conn.disconnect()
+if __name__ == '__main__':
+    ip = sys.argv[1]
+    data_file = sys.argv[2]
+    print(ip)
+    print(data_file)
+
+    conn = None
+    zk = ZK(ip, port=4370, timeout=5)
+    print('Connecting to device ...')
+    conn = zk.connect()
+    try:
+        
+        data = Data()
+
+        conn.enable_device()
+        print('Firmware Version: : {}'.format(conn.get_firmware_version()))
+        conn.test_voice()
+
+        users = conn.get_users()
+        for user in users:
+            cuser = CompleteUser.from_orm(user)
+            data.users.append(cuser)
+            print(cuser.json())
+
+        fps = conn.get_templates()
+        for fp in fps:
+            finger = Fingerprint.from_orm(fp)
+            print(finger.json())
+            assign_to_user(finger, data.users)    
+
+        conn.enable_device()
+
+        with open(data_file, 'w') as f:
+            f.write(data.json())
+
+
+    except Exception as e2:
+        logging.exception(e2)
+        conn.disconnect()
