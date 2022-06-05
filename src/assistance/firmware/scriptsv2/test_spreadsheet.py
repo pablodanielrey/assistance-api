e820af4e66f78dcee8a6fdea321e05c1330d5167
Pablo Daniel Rey <pablodanielrey@gmail.com>
2022-06-05 13:05:40 -0300

    bueno ahora si esta encontrando y creando si no existe

--- src/assistance/firmware/scriptsv2/test_spreadsheet.py
+++ src/assistance/firmware/scriptsv2/test_spreadsheet.py
@@ -2,10 +2,75 @@
 from __future__ import print_function
 
 import datetime
+from mimetypes import MimeTypes
 import os.path
+from syslog import LOG_DEBUG
 
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def get_attlogs_folder_id(drive) -> str:
+    page_token = None
+    items = []
+    while True:
+        results = drive.files().list(q="mimeType='application/vnd.google-apps.folder' and name contains 'attendance_logs'",
+                                    spaces='drive',
+                                    fields="nextPageToken, files(id, name)",
+                                    pageSize=10,
+                                    pageToken=page_token).execute()
+        items.extend(results.get('files', []))
+        page_token = results.get('nextPageToken', None)
+        if not page_token:
+            break
+
+    if not items or len(items) == 0:
+        raise Exception('no se encuentra la carpeta de los logs')
+    return items[0]['id']
+
+
+def get_attlog_file_name() -> str:
+    sname = f"logs_{datetime.datetime.now().date()}"
+    return sname
+
+def get_attlog_file(drive, parent_id: str) -> str:
+
+    sname = get_attlog_file_name()
+
+    page_token = None
+    items = []
+    while True:
+        results = drive.files().list(q=f"mimeType='application/vnd.google-apps.spreadsheet' and trashed = false and name = '{sname}' and '{parent_id}' in parents",
+                                    spaces='drive',
+                                    fields="nextPageToken, files(id, name, parents)",
+                                    pageSize=10,
+                                    pageToken=page_token).execute()
+        items.extend(results.get('files', []))
+        page_token = results.get('nextPageToken', None)
+        if not page_token:
+            break
+
+    if not items or len(items) == 0:
+        raise Exception('no se encuentra el archivo de logs')
+    
+    logging.debug(items)
+    return items[0]['id']
+
+
+def create_attlog_file(drive, parent_id: str) -> str:
+    sname = get_attlog_file_name()
+
+    attlogs_folter_id = parent_id
+    meta = {
+        'mimeType': "application/vnd.google-apps.spreadsheet", 
+        'parents': [attlogs_folter_id],
+        'name': sname
+    }
+    data = drive.files().create(body=meta,fields='id, name, parents, mimeType').execute()
+    logging.debug(data)
+    return data['id']
 
 
 SCOPES = [
@@ -27,23 +92,16 @@ credentials = creds.with_subject('sistemas@econo.unlp.edu.ar')
 #     target_scopes=SCOPES,
 #     lifetime=500)
 
+# service = build('sheets', 'v4', credentials=credentials)
+drive = build('drive', 'v3', credentials=credentials)
+
+folder_id = get_attlogs_folder_id(drive)
 try:
-    # service = build('sheets', 'v4', credentials=credentials)
-    drive = build('drive', 'v3', credentials=credentials)
+    sid = get_attlog_file(drive, folder_id)
+except Exception as e:
+    sid = create_attlog_file(drive, folder_id)
 
-    # Call the Drive v3 API
-    results = drive.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
+print(sid)
 
-    if not items:
-        print('No files found.')
-    print('Files:')
-    for item in items:
-        print(u'{0} ({1})'.format(item['name'], item['id']))
-
-except Exception as error:
-    # TODO(developer) - Handle errors from drive API.
-    print(f'An error occurred: {error}')
 
 
