beef64e41031299bf4615a6abc7e0e57f58739b0
Pablo Daniel Rey <pablodanielrey@gmail.com>
2022-06-12 21:08:44 -0300

    fixed the build system

--- generar_imagen.sh
+++ generar_imagen.sh
@@ -1,2 +1,2 @@
-docker build -t registry.econo.unlp.edu.ar/issues_ingress:v0.0.5 .
-docker push registry.econo.unlp.edu.ar/issues_ingress:v0.0.5
+docker build -t registry.econo.unlp.edu.ar/assistance-api:v2.0.0 .
+docker push registry.econo.unlp.edu.ar/assistance-api:v2.0.0
