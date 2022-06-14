f81f17d74949e79da13d4bb2769fb4762d8ae7e1
Pablo Daniel Rey <pablodanielrey@gmail.com>
2022-06-13 22:37:26 -0300

    increase the version

--- generar_imagen.sh
+++ generar_imagen.sh
@@ -1,2 +1,2 @@
-docker build -t registry.econo.unlp.edu.ar/assistance-api:v2.0.1 .
-docker push registry.econo.unlp.edu.ar/assistance-api:v2.0.1
+docker build -t registry.econo.unlp.edu.ar/assistance-api:v2.0.2 .
+docker push registry.econo.unlp.edu.ar/assistance-api:v2.0.2
