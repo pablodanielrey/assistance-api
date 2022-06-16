918e389bb44b32466a49b4e57c15c3b2e16513df
Pablo Daniel Rey <pablodanielrey@gmail.com>
2022-06-15 21:07:34 -0300

    name of the default service changed

--- src/assistance/infra/redis/settings.py
+++ src/assistance/infra/redis/settings.py
@@ -1,8 +1,12 @@
 from pydantic import BaseSettings
 
 class RedisSettings(BaseSettings):
-
-    redis_host: str = "redis"
+    """
+        Redis settings. see
+        https://github.com/kubernetes/kubernetes/issues/60099
+        https://www.pedidos.econo.unlp.edu.ar/issues/22743
+    """
+    redis_host: str = "assistance_redis"
     redis_port: int = 6379
     redis_db: int = 0
 
